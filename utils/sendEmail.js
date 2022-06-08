5e30d8a45a4e35d2c155efcae1b81c80ca1271af
nmbaibhab <nmbaibhab@gmail.com>
2022-06-09 00:08:59 +0530

    authentication and userSchema configured

---
+++ utils/sendEmail.js
@@ -0,0 +1,33 @@
+const nodemailer = require("nodemailer");
+require("dotenv").config({ path: "./.env" });
+
+const sendEmail = (options) => {
+  const transporter = nodemailer.createTransport({
+    // host: "smtp.gmail.com",
+    // port: 993,
+    // transportMethod: "SMTP",
+    // host: 'smtp.ethereal.email',
+    // port: 587,
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASS,
+    },
+  });
+
+  const mailOptions = {
+    from: process.env.EMAIL,
+    to: options.to,
+    subject: options.subject,
+    html: options.text,
+  };
+  transporter.sendMail(mailOptions, function (err, info) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(info);
+    }
+  });
+};
+
+module.exports = sendEmail;
